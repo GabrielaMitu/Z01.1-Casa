4d40821702d446b0add34ecb5db7965abc66a390
antonioarf <antonioarf@al.insper.edu.br>
2020-11-17 15:37:11 -0300

    code.java

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -16,8 +16,24 @@ public class Code {
      * @return Opcode (String de 4 bits) com código em linguagem de máquina para a instrução.
      */
     public static String dest(String[] mnemnonic) {
-        /* TODO: implementar */
-    	return "";
+          int tamanho = mnemnonic.length;
+
+          if (mnemnonic[0] == "jmp" || mnemnonic[0] == "je" || mnemnonic[0] == "jne" || mnemnonic[0] == "jg" || mnemnonic[0] == "jge" || mnemnonic[0] == "jl" || mnemnonic[0] == "jle"){
+              return "0000";
+          }
+          else if (tamanho <4){
+              if (mnemnonic[tamanho-1] == "(%A)"){ return "0100";}
+              else if (mnemnonic[tamanho-1] == "%A"){ return "0001";}
+              else if (mnemnonic[tamanho-1] == "%D"){ return "0010";}
+          }
+          else if (tamanho == 4)
+              if (mnemnonic[0] == "movw" && mnemnonic[3] == "(%A)")  {return "0110";}
+              if (mnemnonic[0] == "addw" && mnemnonic[3] == "%D")  {return "0010";}
+              else{
+                  if (mnemnonic[tamanho-1] == "(%A)"){ return "0100";}
+                  else if (mnemnonic[tamanho-1] == "%A"){ return "0001";}
+                  else if (mnemnonic[tamanho-1] == "%D"){ return "0010";}}
+            return "0000";
     }
 
     /**
@@ -26,8 +42,108 @@ public class Code {
      * @return Opcode (String de 7 bits) com código em linguagem de máquina para a instrução.
      */
     public static String comp(String[] mnemnonic) {
-        /* TODO: implementar */
-    	return "";
+        if (mnemnonic[0].equals("jmp")){ return "000001100";}
+        else if (mnemnonic[0].equals("je")){ return "000001100"; }
+        else if (mnemnonic[0].equals("jne")){ return "000001100";}
+        else if (mnemnonic[0].equals("jg")){ return "000001100";}
+        else if (mnemnonic[0].equals("jge")){ return "000001100";}
+        else if (mnemnonic[0].equals("jl")){ return "000001100";}
+        else if (mnemnonic[0].equals("jle")){ return "000001100";}
+
+        else if (mnemnonic[0].equals("movw")){
+            if (mnemnonic[1].equals("%A")){
+                switch (mnemnonic[2]){
+                    case "%D"  : return "000110000";
+                    case "(%A)": return "000110000";
+                    default    : return "000000000";
+                }}
+            else if (mnemnonic[1].equals("%D")){
+                switch (mnemnonic[2]){
+                    case "%A"  : return "000001100";
+                    case "(%A)": return "000001100";
+                    default    : return "000000000";
+                }}
+            else if (mnemnonic[1].equals("(%A)")){
+                switch (mnemnonic[2]){
+                    case "%A"  : return "001110000";
+                    case "%D"  : return "001110000";
+                    default    : return "000000000";
+                }}
+        }
+//        SIMPLICANDO
+        else if (mnemnonic[0].equals("addw")){
+            if (mnemnonic[1].equals("%A")){
+                switch (mnemnonic[2]){
+                    case "%D"  : return "000000010";
+                    default    : return "000000000";
+                }}
+            else if (mnemnonic[1].equals("(%A)")){
+                switch (mnemnonic[2]){
+                    case "%D"  : return "001000010";
+                    default    : return "000000000";
+                }}
+            else if (mnemnonic[1].equals("$1")){
+                switch (mnemnonic[2]){
+                    case "(%A)"  : return "001110111";
+                    default    : return "000000000";
+                }}
+        }
+
+        else if (mnemnonic[0].equals("incw")){
+            switch (mnemnonic[1]){
+                case "(%A)"  : return "001110111";
+                case "%A"  : return "000110111";
+                case "%D"  : return "000011111";
+                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("decw")){
+            switch (mnemnonic[1]){
+                case "%A"  : return "000110010";
+                case "%D"  : return "000001110";
+                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("notw")){
+            switch (mnemnonic[1]){
+                case "%A"  : return "000110001";
+                case "%D"  : return "000001101";
+                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("negw")){
+            switch (mnemnonic[1]){
+                case "%A"  : return "000110011";
+                case "%D"  : return "000001111";
+                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("andw")){
+            switch (mnemnonic[1]){
+                case "(%A)"  : return "001000000";
+                case "%D"  : return "000000000";
+//                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("orw")){
+            switch (mnemnonic[1]){
+                case "(%A)"  : return "001010101";
+                case "%D"  : return "000010101";
+                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("subw")){
+            switch (mnemnonic[1]){
+                case "(%A)"  : return "001110010";
+                case "%D"  : return "001010011";
+                default    : return "000000000";
+            }
+        }
+        else if (mnemnonic[0].equals("rsubw")){return "001000111";}
+//        else {
+//            return "";
+//        }
+        return "";
     }
 
     /**
@@ -36,8 +152,17 @@ public class Code {
      * @return Opcode (String de 3 bits) com código em linguagem de máquina para a instrução.
      */
     public static String jump(String[] mnemnonic) {
-        /* TODO: implementar */
-    	return "";
+        switch (mnemnonic[0]){
+            case "jmp"  : return "111";
+            case "jle"  : return "110";
+            case "jne"  : return "101";
+            case "jl"   : return "100";
+            case "jge"  : return "011";
+            case "je"   : return "010";
+            case "jg"   : return "001";
+
+            default    : return "000";
+        }
     }
 
     /**
@@ -46,8 +171,9 @@ public class Code {
      * @return Valor em binário (String de 15 bits) representado com 0s e 1s.
      */
     public static String toBinary(String symbol) {
-        /* TODO: implementar */
-    	return "";
+        int value = Integer.valueOf(symbol);
+        String binary = Integer.toBinaryString(value);
+        return String.format("%1$16s", binary).replace(" ", "0");
     }
 
 }
